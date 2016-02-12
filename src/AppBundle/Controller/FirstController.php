7c1047abd69af11088f184d4c063e237d40729fb
vagrant <vagrant@local.puphpet>
2016-02-12 14:57:13 +0100

    files after provision and FirstController was added.

---
+++ src/AppBundle/Controller/FirstController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class myFirstController extends Controller
+{
+    /**
+     * @Route("/FirstController")
+     */
+    public function myFirstAction()
+    {
+        return new Response("Woohoo my really first own php file on github!")
+    }
+}
